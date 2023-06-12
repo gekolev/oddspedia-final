f0a14a0b6e611a1c2038d964603d0e5de242e7fe
George Kolev <gogo1520180@gmail.com>
2023-06-12 23:01:56 +0300

    basic styles

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HeaderComp />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <MyTeamsComp />
   </div>
 </template>
 
 <script>
 import HeaderComp from "./components/HeaderComp.vue";
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import MyTeamsComp from "./components/MyTeamsComp.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    // HelloWorld,
     HeaderComp,
+    MyTeamsComp,
   },
 };
 </script>
@@ -26,6 +29,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: $frame-color;
+  min-height: 100vh;
 }
 </style>
